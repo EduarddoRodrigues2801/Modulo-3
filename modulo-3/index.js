b6d5fff33fdeb71a27641477009ef835be3f29fd
EduarddoRodrigues2801 <103973655+EduarddoRodrigues2801@users.noreply.github.com>
2022-08-02 17:28:38 -0300

    Add files via upload

---
+++ modulo-3/index.js
@@ -0,0 +1,26 @@
+import { CadastroComponent } from "./controller/cadastro.js";
+import { FaturamentoComponent } from "./controller/faturamento.js";
+import { ListaClienteComponent } from "./controller/lista-Clientes.js";
+
+const link = document.getElementById('link')
+
+link.addEventListener('click', (event) =>{
+   const option = event.path[0].innerText
+
+   switch(option){
+      case "Cadastro":
+         CadastroComponent();
+         break
+      case "Clientes":
+         ListaClienteComponent();
+         break
+      case "Checkin": 
+         window.location.href = "./checkin.html"
+         break  
+      case "Faturamento": 
+         FaturamentoComponent();
+         break
+          
+         
+   }
+})
